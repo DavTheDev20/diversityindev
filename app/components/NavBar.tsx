f96cb49f044ed6127306548c8ff84fb60380f8ba
DavTheDev20 <reiddavin@outlook.com>
2024-01-13 17:22:59 -0500

    Add navigation drawer

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,16 +1,43 @@
+"use client";
+
+import NavDrawer from "./NavDrawer";
+import { useState } from "react";
+
 export default function NavBar() {
+  const [navDrawerToggled, setNavDrawerToggled] = useState(false);
+
   return (
-    <nav className="bg-violet-600 h-20">
-      <h3 className="text-white float-left mt-6 ml-10 font-semibold text-2xl">
+    <nav className="bg-violet-600 h-20 relative z-10">
+      <a
+        href="/"
+        className="text-white float-left mt-6 ml-10 font-semibold text-2xl"
+      >
         Diversity In Dev
-      </h3>
-      <div className="flex justify-end align-middle">
-        <div id="hambuger" className="pr-6 pt-6">
-          <div className="bg-white w-10 h-1 mb-2"></div>
-          <div className="bg-white w-10 h-1 mb-2"></div>
-          <div className="bg-white w-10 h-1"></div>
+      </a>
+      {!navDrawerToggled ? (
+        <div className="flex justify-end align-middle">
+          <div
+            id="hambuger"
+            className="pr-6 pt-6"
+            onClick={() => setNavDrawerToggled(true)}
+          >
+            <div className="bg-white w-10 h-1 mb-2"></div>
+            <div className="bg-white w-10 h-1 mb-2"></div>
+            <div className="bg-white w-10 h-1"></div>
+          </div>
         </div>
-      </div>
+      ) : null}
+
+      {navDrawerToggled ? (
+        <NavDrawer
+          toggleFunction={() => setNavDrawerToggled(false)}
+          links={[
+            { linkName: "Login", linkLocation: "/login" },
+            { linkName: "About", linkLocation: "/about" },
+            { linkName: "Blog", linkLocation: "/blog" },
+          ]}
+        />
+      ) : null}
     </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import NavDrawer from "./components/NavDrawer";
 
 export default function Home() {
   return <main className=""></main>;
