bcc2f31229bc377163ef0bcf3688ed605865da60
Davin@Lenovo730 <reiddavin@outlook.com>
2024-01-15 19:12:40 -0500

    Seperate authOptions to an individual file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-
-const authOptions = {
-  secret: "qbpZDy9NiMgf_foYDZrP3AhgiRxq3uA2EO7pXKePnyM",
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-};
+import authOptions from "@/app/authOptions";
 
 const handler = NextAuth(authOptions);
 
