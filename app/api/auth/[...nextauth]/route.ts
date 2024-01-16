9feb5201e71835c10c30df012a18502a43fda2fa
Davin@Lenovo730 <reiddavin@outlook.com>
2024-01-15 19:03:12 -0500

    Remove export from authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   secret: "qbpZDy9NiMgf_foYDZrP3AhgiRxq3uA2EO7pXKePnyM",
   providers: [
     GithubProvider({
